274c5b66a6ef39b6d67af0c9bc17239990d0395f
emptinessbliss15-cell <emptinessbliss15@gmail.com>
2026-08-26 12:38:32 -0400

    test checkbox update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+	// Checkbox implementation placeholder: preserve existing working grid behavior.
+	// TODO: integrate the Complete checkbox into the existing RevoGrid columns.
+</script>
