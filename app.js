a5a9cfa1ba0174552f00fbec7ece465e04af32c4
emptinessbliss15-cell <emptinessbliss15@gmail.com>
2026-09-04 10:15:31 -0400

    Use ComboBox for New Holon type

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ async function createHolon(prefillName = '', prefillType = '') {
     submitLabel: 'Create Holon',
     fields: [
       { name: 'name', label: 'Name', required: true, placeholder: 'Holon name', value: prefillName },
-      { name: 'holon_type', label: 'Type', type: 'select', options: holonTypeOptions(type), value: type, required: true },
+      { name: 'holon_type', label: 'Type', type: 'combobox', options: holonTypeOptions(type), value: type, required: true, minChars: 0, allowCustom: false, placeholder: 'Find a Holon type…' },
       { name: 'relationship_type_id', label: 'Initial Relationship', type: 'select', options: relationshipTypeOptions(true), value: '' },
       { name: 'parent_holon_id', label: 'Parent Holon', type: 'select', options: holonOptions(true), value: '' },
       { name: 'position', label: 'Position', type: 'number', value: '0' },
